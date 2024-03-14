fe16094f15aa9ceabeae8b87481c6912d73c3729
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-03-14 12:14:10 +0900

    [level 0] Title: 가위 바위 보, Time: 2.16 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        System.out.println(rsp.length());
+        for (int i = 0; i < rsp.length(); i++) {
+            if (rsp.charAt(i) == '2') {
+                answer = answer + "0";
+            } else if (rsp.charAt(i) == '0') {
+                answer = answer + "5";
+            } else { answer = answer + "2"; }
+        }
+        return answer;
+    }
+}
