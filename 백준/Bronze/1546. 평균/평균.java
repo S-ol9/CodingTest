d2b53cf7715786c39aa0c3bb38fb347ceb244862
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-03-18 14:34:47 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 19328 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,16 +5,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[] arr = new int[N];
-        long max = 0;
-        long sum = 0;
-        for(int i =0; i<N; i++) {
+        double[] arr = new double[N];
+        double max = 0;
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
             if (max < arr[i]) {
                 max = arr[i];
             }
-            sum += arr[i];
         }
-        System.out.println((double)sum * 100 / max / N);
+        for (int i = 0; i < N; i++) {
+           arr[i] = arr[i] / max * 100;
+           sum += arr[i];
+        }
+        System.out.println(sum / N);
     }
 }
