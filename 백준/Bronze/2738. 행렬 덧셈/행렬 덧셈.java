7e5e5d91a8b92cd06bf7b6fcaf0617c557e7e74f
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-03-19 13:37:17 +0900

    Update 행렬 덧셈.java

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -6,27 +6,27 @@ public class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        int[] A = new int[N*M];
-        int[] B = new int[N*M];
-        for(int i = 0; i<N*M; i++) {
+        int[] A = new int[N * M];
+        int[] B = new int[N * M];
+        for (int i = 0; i < N * M; i++) {
             A[i] = sc.nextInt();
         }
-        for(int i =0; i <N*M; i++) {
+        for (int i = 0; i < N * M; i++) {
             B[i] = sc.nextInt();
         }
 
-        int[] answer = new int[N*M];
-        for(int i = 0; i < N * M; i++) {
+        int[] answer = new int[N * M];
+        for (int i = 0; i < N * M; i++) {
             answer[i] = A[i] + B[i];
         }
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 System.out.print(answer[i * M + j] + " ");
             }
             System.out.println();
         }
     }
-    
+
     // 이차원 배열 사용하기
 }
