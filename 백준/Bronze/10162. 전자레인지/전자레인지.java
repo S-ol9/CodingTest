87a622bc630dc0208660d1f1afeea77c21ef5387
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-04-03 00:23:13 +0900

    Update 전자레인지.java

--- 백준/Bronze/10162. 전자레인지/전자레인지.java
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -3,29 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long T = sc.nextLong();
+        int T = sc.nextInt();
 
-        long A = 300;
-        long B = 60;
-        long C = 10;
-
-        long sA = T / A;
-        long sB = (T % A) / B;
-        long sC = ((T % A) % B) / C;
-        if (T % A == 0) {
-            sB = 0;
-            sC = 0;
-            System.out.println(sA + " " + sB + " " + sC);
+        if (T % 10 != 0) {
+            System.out.println(-1);
         } else {
-            if ((T % A) % B == 0) {
-                System.out.println(sA + " " + sB + " " + sC);
-            } else {
-                if (((T % A) % B) % C == 0) {
-                    System.out.println(sA + " " + sB + " " + sC);
-                } else {
-                    System.out.println(-1);
-                }
-            }
+            int A = T / 300;
+            T = T % 300;
+            int B = T / 60;
+            T = T % 60;
+            int C = T / 10;
+            System.out.println(A + " " + B + " " + C);
         }
     }
 }
