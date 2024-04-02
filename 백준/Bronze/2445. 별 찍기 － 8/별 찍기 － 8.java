19cbf1f29a154ce625641103278a1ec4e0ae96fd
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-04-02 14:14:53 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 612 ms, Memory: 23772 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N * 2; j++) {
+                if (j <= i) {
+                    System.out.print("*");
+                } else {
+                    if (N * 2 - i >= j) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 1; j <= N * 2; j++) {
+                if (i >= j) {
+                    System.out.print("*");
+                } else {
+                    if (N * 2 - i >= j) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
