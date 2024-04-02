d5f0edc7480ab82f7406e693fcc7a2bd4874056b
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-04-03 00:55:59 +0900

    Update 별 찍기 － 12.java

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for(int i = 1 ; i <= N; i++) {
-            for(int j = 1; j <= N; j++) {
-                if(N-i>=j) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (N - i >= j) {
                     System.out.print(" ");
                 } else {
                     System.out.print("*");
@@ -15,9 +15,9 @@ public class Main {
             System.out.println();
         }
 
-        for(int i = N-1; i>0; i--) {
-            for(int j = 1; j<=N; j++) {
-                if(N-i<j) {
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 1; j <= N; j++) {
+                if (N - i < j) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
