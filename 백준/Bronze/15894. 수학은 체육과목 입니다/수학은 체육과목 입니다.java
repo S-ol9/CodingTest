b91f86cef2f5c1dac7d7ef9b0f84ef2b7800e401
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-04-03 09:26:00 +0900

    Update 수학은 체육과목 입니다.java

--- 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long N = sc.nextInt();
+        long N = sc.nextLong();
 
         System.out.println(N*4);
     }
