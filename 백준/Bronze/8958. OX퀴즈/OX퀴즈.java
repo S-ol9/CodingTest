907bf8a533175ed22e69746a1c9114b7e026ab3d
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-04-08 10:10:03 +0900

    [Bronze II] Title: OX퀴즈, Time: 228 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String test = "";
+        int count;
+        int sum;
+        for(int i = 0; i < N; i++) {
+            count = 0;
+            sum = 0;
+            ArrayList<String> st = new ArrayList<>();
+            test = sc.next();
+            for(int j = 0; j<test.length(); j++) {
+                if(test.charAt(j) =='O') {
+                    count++;
+                    sum += count;
+                } else {
+                    count = 0;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
