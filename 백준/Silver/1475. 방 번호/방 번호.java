8c9129a2fd92bffda3cff72feea31e3abb6196a8
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-06-20 09:48:08 +0900

    [Silver V] Title: 방 번호, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        int[] arr = new int[9];
+
+        for (int i = 0; i < N.length(); i++) {
+            int num = Character.getNumericValue(N.charAt(i));
+            if (num == 9) {
+                num = 6;
+            }
+            arr[num]++;
+        }
+        arr[6] = arr[6] / 2 + arr[6] % 2;
+
+        Arrays.sort(arr);
+        System.out.println(arr[8]);
+    }
+}
