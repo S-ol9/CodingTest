1645ea5296d2fdbd543ca99b6c9cb600edafe598
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-07-02 10:45:15 +0900

    [Silver V] Title: 스네이크버드, Time: 212 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(sc.nextLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (L >= arr[i]) {
+                L++;
+            }
+        }
+
+        System.out.println(L);
+    }
+}
