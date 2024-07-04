f70bb6958475def27332eea0b37c528637634373
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-07-05 00:03:45 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,20 +1,30 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i =0; i<N; i++) {
-            arr.add(sc.nextInt());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
         }
-        Collections.sort(arr);
 
-        for(int e : arr) {
-            System.out.println(e);
+        // 버블 정렬
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.println(A[i]);
         }
     }
+
 }
