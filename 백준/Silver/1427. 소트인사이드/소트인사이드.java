e2bd6886dd6818f476da42e9f441250f730fe079
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-07-05 00:33:51 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,24 +1,36 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        char[] charr = br.readLine().toCharArray();
-        ArrayList<Integer> iarr = new ArrayList<>();
-        for(char e : charr) {
-            iarr.add(Character.getNumericValue(e));
+        String str = br.readLine();
+        int[] A = new int[str.length()];
+        for(int i = 0 ; i < A.length; i++) {
+            A[i] = Integer.parseInt(str.substring(i, i+1));
         }
 
-        Collections.sort(iarr, Collections.reverseOrder());
-
-        for(Integer e : iarr) {
-            bw.write(e+"");
+        // 선택 정렬
+        for(int i = 0 ; i < A.length; i++) {
+            int max = i; // 인덱스
+            for(int j = i+1; j < A.length; j++) {
+                // 최댓값 찾기
+                if(A[j] > A[max]) {
+                    max = j;
+                }
+            }
+            
+            if(A[i] < A[max]) {
+                int temp = A[i];
+                A[i] = A[max];
+                A[max] = temp;
+            }
         }
-        bw.flush();
-        bw.close();
+
+        for(int i : A) {
+            System.out.print(i);
+        }
     }
 }
