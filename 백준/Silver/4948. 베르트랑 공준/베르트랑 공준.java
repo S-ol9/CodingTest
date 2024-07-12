7de8410a9e71c29918e076170ff0b3c2dad4ba10
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-07-13 00:18:12 +0900

    [Silver II] Title: 베르트랑 공준, Time: 360 ms, Memory: 33204 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,26 +6,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            int count = 0;
             int n = sc.nextInt();
             if (n == 0) {
                 break;
             }
             int t = n * 2;
-            boolean bool = false;
-            for (int i = n + 1; i <= t; i++) {
-                int num = (int) Math.sqrt(i);
-                for (int j = 2; j <= num; j++) {
-                    if (i % j == 0) { // 소수가 아님
-                        bool = true;
-                        break;
+
+            // 에라토스테네스의 체 알고리즘
+            // 2부터 시작하여 배수값을 찾아 false로 지정하여 소수 판별
+            boolean[] bool = new boolean[t + 1];
+            Arrays.fill(bool, true); // 배열값 전체 true로 변경
+            bool[0] = false;
+            bool[1] = false;
+
+            for (int i = 2; i * i <= t; i++) {
+                if (bool[i]) { // 만약 true 라면
+                    for (int j = i * i; j <= t; j += i) {
+                        bool[j] = false; // i의 배수 = 소수 X
                     }
                 }
-                if (!bool) { // 소수가 아니라면
+            }
+            int count = 0;
+            for (int i = n + 1; i <= t; i++) {
+                if (bool[i]) {
                     count++;
                 }
-                bool = false;
             }
+
             System.out.println(count);
         }
     }
