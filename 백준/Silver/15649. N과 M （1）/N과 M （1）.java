39ca0a44892527276c463eeda9700c486847345d
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-07-14 20:49:23 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 23432 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] bool;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); // 배열 길이
+
+        bool = new boolean[N];
+        arr = new int[M];
+
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!bool[i]) { // false 라면 == 방문 X
+                bool[i] = true; // true로 변경 == 방문 O
+                arr[depth] = i + 1; // 해당 변수는 depth
+                dfs(depth + 1);
+                bool[i] = false; // 자식 노드 방문 후 방문 X 상태로 변경
+            }
+        }
+    }
+}
