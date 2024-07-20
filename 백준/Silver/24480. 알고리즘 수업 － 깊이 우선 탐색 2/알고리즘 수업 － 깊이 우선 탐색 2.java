0a65394f9d08a52d98f0740b4d3f3b0c896cf6f7
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-07-20 15:46:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1036 ms, Memory: 106940 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -6,58 +6,44 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>(); // 노드와 간선 표현 그래프
-    static int[] visited; // 방문 노드 체크 배열
-    static int count; // 방문 순서
+    static int N, M, R, count = 1;
+    static int[] arr; // 결과 저장 배열
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()); // 정점 개수
-        int M = Integer.parseInt(st.nextToken()); // 간선 개수
-        int R = Integer.parseInt(st.nextToken()); // 처음 노드
+        arr = new int[N + 1];
+        visited = new boolean[N + 1];
 
-        visited = new int[N + 1];
-
-        // 첫 시작 -> 시작과 연결된 노드 중 가장 작은 숫자 탐색 -> 탐색한 숫자는 제외
-        for (int i = 0; i <= N; i++) {
-            // 각 노드별 빈 리스트 생성
-            graph.add(new ArrayList<>());
-        }
+        for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
 
-        for (int i = 0; i < graph.size(); i++) {
-            Collections.sort(graph.get(i), Collections.reverseOrder());
-        }
-
-        count = 1; // visted 배열과 혼동이 올 수 있음.
         dfs(R);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) sb.append(arr[i]).append("\n");
 
-        // visited에 몇 번째로 출력되는지 출력 => 방문 순번!!
-        for (int i = 1; i < visited.length; i++) {
-            System.out.println(visited[i]);
-        }
-
+        System.out.println(sb);
     }
 
-    private static void dfs(int start) {
-        visited[start] = count;
-        for (int i = 0; i < graph.get(start).size(); i++) {
-            int y = graph.get(start).get(i);
+    private static void dfs(int R) {
+        visited[R] = true;
+        arr[R] = count++;
+        Collections.sort(graph.get(R), Collections.reverseOrder());
 
-            if (visited[y] == 0) {
-                count++;
-                dfs(y);
-            }
-        }
+        for (Integer val : graph.get(R)) if (!visited[val]) dfs(val);
     }
 }
