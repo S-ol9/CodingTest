2b7091ffc05f4350d349ee36949133592871775c
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-08-07 10:45:59 +0900

    [Silver IV] Title: 덱 2, Time: 1156 ms, Memory: 260508 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> dq = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "1":
+                    dq.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "2":
+                    dq.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "3":
+                    sb.append(dq.isEmpty() ? -1 : dq.removeFirst()).append("\n");
+                    break;
+                case "4":
+                    sb.append(dq.isEmpty() ? -1 : dq.removeLast()).append("\n");
+                    break;
+                case "5":
+                    sb.append(dq.size()).append("\n");
+                    break;
+                case "6":
+                    sb.append(dq.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "7":
+                    sb.append(dq.isEmpty() ? -1 : dq.getFirst()).append("\n");
+                    break;
+                case "8":
+                    sb.append(dq.isEmpty() ? -1 : dq.getLast()).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
