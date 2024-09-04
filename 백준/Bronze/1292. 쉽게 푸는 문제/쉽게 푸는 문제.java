9717f86ab3c0c07fb2db793f11687ffda2044030
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-09-04 09:30:11 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 108 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,19 +10,19 @@ public class Main {
 
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-        List<Integer> list = new ArrayList<>();
+
+        int cnt = 0; // 반복 체크
+        int ans = 0; // 누적 합
 
         for (int i = 1; i <= B; i++) {
             for (int j = 0; j < i; j++) {
-                list.add(i);
+                cnt++;
+
+                if (A <= cnt && cnt <= B) { // 반복 횟수가 A와 B 사이
+                    ans += i;
+                }
             }
         }
-
-        int sum = 0;
-        for (int i = A - 1; i < B; i++) {
-            sum += list.get(i);
-        }
-
-        System.out.println(sum);
+        System.out.println(ans);
     }
 }
