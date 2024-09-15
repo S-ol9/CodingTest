7a65ace361f6807fa50f8beedfe1aa0a457c76a1
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-09-16 01:07:38 +0900

    [Bronze IV] Title: 모음의 개수, Time: 100 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
