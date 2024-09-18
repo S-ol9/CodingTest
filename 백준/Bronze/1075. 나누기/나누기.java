90453f8705118b8fba87238f92a4bf9111e60d72
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-09-18 22:18:11 +0900

    [Bronze II] Title: 나누기, Time: 108 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int F = Integer.parseInt(br.readLine());
+
+        N -= (N % 100); // 뒤 두자리 수 00 변경
+
+        while (true) {
+            if (N % F == 0) {
+                break;
+            }
+            N++;
+        }
+        System.out.format("%02d", (N % 100)); // 형식 지정
+    }
+}
