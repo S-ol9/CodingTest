b2ff249e3fcc0e68917c3f9247b19a91d47a7ab1
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-10-20 00:21:24 +0900

    [Gold V] Title: 돌 게임 6, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long N = Long.parseLong(br.readLine());
+
+        // 규칙성 찾기
+        System.out.println(N % 7 == 0 || N % 7 == 2 ? "CY" : "SK");
+    }
+}
