7c077639b77bfb3bcf490da62881d912af3fc162
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-10-19 19:37:02 +0900

    [Silver V] Title: 생일, Time: 92 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][4];
+
+        StringTokenizer st;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken(); // name
+            arr[i][1] = st.nextToken(); // day
+            arr[i][2] = st.nextToken(); // month
+            arr[i][3] = st.nextToken(); // year
+        }
+        
+        // 내림차순 정렬
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if(o1[3].equals(o2[3])) {
+                    if(o1[2].equals(o2[2])) {
+                        return Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]);
+                    } else {
+                        return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+                    }
+                } else {
+                    return Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]);
+                }
+            }
+        });
+
+        System.out.println(arr[n-1][0]); // 나이가 가장 적은 사람
+        System.out.println(arr[0][0]); // 나이가 가장 많은 사람
+    }
+}
