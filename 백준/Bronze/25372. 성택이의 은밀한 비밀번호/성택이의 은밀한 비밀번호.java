7ed76b57333cb45cdfd672bf2414fbb5a37569e7
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-10-27 00:18:51 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 112 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            if (str.length() > 5 && str.length() < 10) {
+                sb.append("yes").append("\n");
+            } else {
+                sb.append("no").append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
