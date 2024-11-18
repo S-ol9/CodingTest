79103b6abc7d0fa78e36179490be13734f190ffc
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-11-18 11:14:32 +0900

    [Bronze IV] Title: 주사위, Time: 132 ms, Memory: 14840 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+
+            sb.append("Case " + i + ": ").append(x1 + x2).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
