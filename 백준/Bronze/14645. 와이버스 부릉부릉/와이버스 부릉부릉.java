bed9b367b8bd4efbe50b0cbbd455a37e727fc448
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-11-28 10:13:14 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 96 ms, Memory: 13936 KB -BaekjoonHub

--- 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i<N; i++){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int sum = K;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sum += A - B;
         }
-
         System.out.println("비와이");
     }
 }
