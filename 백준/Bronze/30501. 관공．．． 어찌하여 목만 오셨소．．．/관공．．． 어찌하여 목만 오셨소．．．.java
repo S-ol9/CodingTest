641ddca946631c347852fb5e7da8428027b2af50
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2024-12-01 01:37:57 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String answer = "";
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if (str.contains("S")) {
+                answer = str;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
