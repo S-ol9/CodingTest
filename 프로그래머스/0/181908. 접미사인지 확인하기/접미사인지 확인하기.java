d8cdb8e42fa8c7bb09490858abe6946ba547ca35
Lee Jinsol <126231366+S-ol9@users.noreply.github.com>
2025-02-09 22:59:49 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int len = my_string.length();
+        for(int i = 0; i < len; i++) {
+            if(my_string.endsWith(is_suffix)) return 1;
+        }
+        return 0;
+    }
+}
